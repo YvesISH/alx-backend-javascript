7aa20d8b45e324a5696e2dfee1b64f9a8cc6a720
root <y.ishimwe2@alustudent.com>
2023-07-05 07:03:10 -0700

    Main

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -17,3 +17,48 @@ const studentB: student = {
   location: "Musanze, Rwanda",
 };
 
+const studentsList: Array<Student> = [
+  studentA,
+  studentB,
+];
+const stylsheet = `
+   html {
+    margin: 0;
+    height: 100%;
+  }
+  body {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    margin: 10%;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  thead {
+    font-weight: bold;
+  }
+  td {
+    padding: 10px;
+    border: 1px solid gray;
+    cursor: pointer;
+  }
+  td:hover {
+    background: gainsboro;
+  }
+
+  td:nth-child(1) {
+    text-align: center;
+  }
+`;
+
+/** Displays info about students in a table */
+
+export const displayStudents = (students: Array<Student>): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+}
