2309af0e62812999d2feee6b0441e6a06bb86ad8
root <y.ishimwe2@alustudent.com>
2023-07-05 03:00:03 -0700

    Main

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,19 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+const studentA: student = {
+  firstName: "Yves",
+  lastName: "Ish",
+  age: 15,
+  location: "Kigali, Rwanda", 
+};
+const studentB: student = {
+  firstName: "Ivan",
+  lastName: "Inn",
+  age: 18,
+  location: "Musanze, Rwanda",
+};
+
